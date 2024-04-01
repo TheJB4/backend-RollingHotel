3b18f2fc736ab8d17103d0c5290d6f58b450962b
Cacho Medina <zanlorenzocacho10@gmail.com>
2024-04-01 20:18:06 -0300

    se agrego router de habitacion a index

--- index.js
+++ index.js
@@ -24,4 +24,4 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "/public")));
 
-app.use("/api/admin", habitacionRouter);
+app.use("/api", habitacionRouter);
