e7f378c07259c00ac54e2a083024134ab76dc38e
Cacho Medina <zanlorenzocacho10@gmail.com>
2024-04-13 23:40:13 -0300

    configuracion de jwt lista

--- src/helpers/jwt/validarJWT.js
+++ src/helpers/jwt/validarJWT.js
@@ -2,16 +2,12 @@ import jwt from "jsonwebtoken";
 import "dotenv/config";
 
 const validarJWT = (req, res, next) => {
-    //recibir el token
-    console.log(req.header);
     const token = req.header("x-token");
     if (!token) {
-        //401 error en la autenticacion
         return res.status(401).json({
             mensaje: "No hay token en la peticion",
         });
     }
-    // si el token existe
     try {
         const payload = jwt.verify(token, process.env.JWT_SECRET_KEY);
         req._id = payload.uid;
@@ -19,7 +15,6 @@ const validarJWT = (req, res, next) => {
         next();
     } catch (error) {
         console.error("Error al verificar el token:", error.message);
-        //error 401 es no autorizado
         if (error.name === "JsonWebTokenError") {
             return res.status(401).json({
                 mensaje: "Token no valido",
